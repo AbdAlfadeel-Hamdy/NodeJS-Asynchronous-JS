12007a57ca36fdfc11049aeb972a8db4e9e19baa
AbdAlfadeel-Hamdy <abdalfadeelh@gmail.com>
2023-03-04 13:46:50 +0200

    async/await

--- index.js
+++ index.js
@@ -17,18 +17,35 @@ const writeFilePro = (fileName, data) =>
     });
   });
 
-readFilePro(`${__dirname}/dog.txt`)
-  .then((data) => {
+// readFilePro(`${__dirname}/dog.txt`)
+//   .then((data) => {
+//     console.log(`Breed: ${data}`);
+//     return superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
+//   })
+//   .then((res) => {
+//     console.log(res.body.message);
+//     return writeFilePro("dog-img.txt", res.body.message);
+//   })
+//   .then(() => {
+//     console.log("Random dog img saved to a file!");
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
+
+const getDogPic = async () => {
+  try {
+    const data = await readFilePro(`${__dirname}/dog.txt`);
     console.log(`Breed: ${data}`);
-    return superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
-  })
-  .then((res) => {
+    const res = await superagent.get(
+      `https://dog.ceo/api/breed/${data}/images/random`
+    );
     console.log(res.body.message);
-    return writeFilePro("dog-img.txt", res.body.message);
-  })
-  .then(() => {
+    await writeFilePro("dog-img.txt", res.body.message);
     console.log("Random dog img saved to a file!");
-  })
-  .catch((err) => {
+  } catch (err) {
     console.log(err);
-  });
+  }
+};
+
+getDogPic();
